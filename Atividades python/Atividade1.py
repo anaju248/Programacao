cb116cdae129418f10fd82e18a23967b63d062ce
anaju248 <ana_ulrich@estudante.sesisenai.org.br>
2026-02-11 14:50:29 +0000

    update

---
+++ Atividades python/Atividade1.py
@@ -0,0 +1,6 @@
+#Pimeira atividade do python usando concatenação de valores
+print("Ola mundo ")
+numero1 = input("digite o número 1: ")
+numero2 = input("digite o número 2: ")
+soma = numero1+numero2
+print("a soma do número 1 e numero 2 é: ",soma) 
---
+++ Atividades python/Atividade2.py
@@ -0,0 +1,13 @@
+#atividade somando valores flutuantes
+numero1 = float(input("insira o numero 1"))
+numero2 = float(input("insira o numero 2"))
+soma = numero1+numero2
+print("A soma dos dois numeros é: ",soma)
+
+#Atividade mediade 4 notas dos alunos
+nota1 = float(input("insira a primeira nota"))
+nota2 = float(input("insira a segunda nota"))
+nota3 = float(input("insira a terceira nota"))
+nota4 = float(input("insira a quarta nota"))
+media = (nota1+nota2+nota3+nota4)/4
+print("a media do aluno é: ",media)
