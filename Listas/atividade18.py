136047314b1be5efda93368ce7bb5c859a02e28d
anaju248 <ana_ulrich@estudante.sesisenai.org.br>
2026-03-11 14:20:09 +0000

    update

--- Listas/atividade18.py
+++ Listas/atividade18.py
@@ -3,11 +3,9 @@ hora_extra = 15
 hora_normal = int(input("Escreva as horas normais trabalhadas ")) 
 hora_extra = int(input("Escreva as horas extras trabalhadas ")) 
 
-salario_bruto = (hora_normal * 10) + hora_extra * 15
+salario_bruto = (hora_normal * 10) + (hora_extra * 15)
 print(f"O salario bruto é: {salario_bruto} ")
-sl = (hora_normal * 10) * 0.10
-sl = (hora_normal * 10) - sl
-sl = (hora_extra * 10) * 0.10
+sb = (hora_normal * 10) * 0.10
+sb = (hora_normal * 10) - sb
 
-
-print(f"O salario liquido é: {sl}")
+print(f"O salario liquido é: {sb}")
--- Listas/atividade19.py
+++ Listas/atividade19.py
@@ -1,2 +1,3 @@
-ai = 4.00
-aa = 3.50
+frango = float(input("Digite o número de frengos que você tem "))
+total = (frango * 7.00) + frango * 4.00
+print("o valaor total a pagar é ", total)
